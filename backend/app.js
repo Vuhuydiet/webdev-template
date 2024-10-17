e85685cc29db9d3dda9e3f126491086129817b38
vuhuydiet <99078820+Vuhuydiet@users.noreply.github.com>
2024-10-17 23:19:32 +0700

    Revert "Add prisma (#1)" (#2)
    This reverts commit d3c49bf4362afe3c259b8d52ec7da4f66d18f426.

--- backend/app.js
+++ backend/app.js
@@ -1,41 +1,29 @@
 import dotenv from 'dotenv';
 dotenv.config();
+import serveInfo from './lib/getServeInfo.js'
 
 import express from 'express';
 const app = express();
 import cors from 'cors';
 
-import morgan from 'morgan';
-import helmet from 'helmet';
-import compression from 'compression';
-
-import { NODE_ENV } from './configs/serve.config.js'
-
 // import routes
-import router from './routes';
+
 
 // middlewares
 // app.use(cors());
-app.use(morgan(NODE_ENV === 'production' ? 'combined' : 'dev'));
-app.use(helmet());
-app.use(compression);
-
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+// static assets
+app.use(express.static(serveInfo.servePath));
+app.get('/', (req, res) => {
+  res.sendFile(serveInfo.serveIndexPath);
+});
+
 // routes
-app.use('/', router);
 
+// server
 const PORT = process.env.PORT || 3000;
-
-const server = app.listen(PORT, () => {
+app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
-
-server.on('SIGINT', () => {
-  server.close(() => {
-    console.log('Server is closed.');
-  });
-});
-
-
--- backend/services/auth.js
+++ backend/services/auth.js
@@ -4,10 +4,10 @@ import fs from 'fs';
 import { getPathFromBackend } from '../lib/getPath.js';
 import passport from 'passport';
 import { Strategy as JwtStrategy, ExtractJwt } from 'passport-jwt';
-import db from '../dbs/queries.js';
+import db from '../models/queries.js';
 
 const PUB_KEY = fs.readFileSync(
-  getPathFromBackend('configs/public_key.pem'),
+  getPathFromBackend('config/public_key.pem'),
   'utf8',
 );
 
@@ -20,8 +20,8 @@ const opts = {
 passport.use(
   new JwtStrategy(opts, async (jwt_payload, done) => {
     try {
-      const userId = jwt_payload.sub;
-      const user = await db.getUserById(userId);
+      const user_id = jwt_payload.sub;
+      const user = await db.getUserById(user_id);
       if (!user) 
         return done(null, false);
       return done(null, user);
